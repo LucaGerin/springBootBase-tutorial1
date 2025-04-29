651678940e57a6189134a0416c68fe943dfd4a79
Luca Gerin <79710969+LucaGerin@users.noreply.github.com>
2025-04-29 10:24:43 +0200

    Edited PlayersController to enable validation

--- src/main/java/com/spring_tutorial/spring_tutorial_1/controllers/PlayersController.java
+++ src/main/java/com/spring_tutorial/spring_tutorial_1/controllers/PlayersController.java
@@ -1,7 +1,10 @@
 package com.spring_tutorial.spring_tutorial_1.controllers;
 
 import com.spring_tutorial.spring_tutorial_1.data.resources.PlayerDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 /* In Spring Boot una classe controller, che si crea tramite l'annotazione @RestController,
@@ -13,20 +16,30 @@ Le principali funzioni sono:
 */
 @RestController
 @RequestMapping("/players") //@RequestMapping che viene utilizzata per definire la radice delle rotte che verranno definite all'interno della classe
+@Validated //Per abilitare la validazione
 public class PlayersController {
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public PlayerDTO findPlayers(
-            @RequestParam(value = "name") String name,
-            @RequestParam(value = "surname") String surname,
-            @RequestParam(value = "yearBirth") Integer year
+    public PlayerDTO findPlayer(
+            @RequestParam(value = "name")
+            String name,
+
+            @RequestParam(value = "surname")
+            String surname,
+
+            @Positive(message ="L'anno deve essere positivo!") //Applicare il vincolo
+            @RequestParam(value = "yearBirth")
+            Integer year
     ) {
         return new PlayerDTO(name, surname, year);
     }
 
+
     @PostMapping
-    public PlayerDTO createPlayer(@RequestBody PlayerDTO request) {
+    public PlayerDTO createPlayer(@Valid //per abilitare la verifica dei vincoli specificati
+                                      @RequestBody
+                                        PlayerDTO request) {
         return new PlayerDTO(request.getName(), request.getSurname(), request.getYearBirth());
     }
 
