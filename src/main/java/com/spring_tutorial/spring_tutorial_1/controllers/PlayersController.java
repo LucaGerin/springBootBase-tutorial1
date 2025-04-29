4e66017296f8c42720736610319828808f62a08d
Luca Gerin <79710969+LucaGerin@users.noreply.github.com>
2025-04-29 10:14:51 +0200

    Added POST method to add a player

--- src/main/java/com/spring_tutorial/spring_tutorial_1/controllers/PlayersController.java
+++ src/main/java/com/spring_tutorial/spring_tutorial_1/controllers/PlayersController.java
@@ -4,8 +4,15 @@ import com.spring_tutorial.spring_tutorial_1.data.resources.PlayerDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+/* In Spring Boot una classe controller, che si crea tramite l'annotazione @RestController,
+può definire una o più rotte tramite le quali sarà possibile accedere alle risorse.
+Le principali funzioni sono:
+- Reindirizzamento a metodi che implementano la logica di business
+- Validazione dell'input
+- Gestione degli errori
+*/
 @RestController
-@RequestMapping("/players")
+@RequestMapping("/players") //@RequestMapping che viene utilizzata per definire la radice delle rotte che verranno definite all'interno della classe
 public class PlayersController {
 
     @GetMapping
@@ -16,6 +23,11 @@ public class PlayersController {
             @RequestParam(value = "yearBirth") Integer year
     ) {
         return new PlayerDTO(name, surname, year);
+    }
+
+    @PostMapping
+    public PlayerDTO createPlayer(@RequestBody PlayerDTO request) {
+        return new PlayerDTO(request.getName(), request.getSurname(), request.getYearBirth());
     }
 
 }
